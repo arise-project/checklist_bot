b1dc820ce2829c8ec8c8f95e76d9a9f916b37f0f
arise-project <pirogov.e@gmail.com>
2020-02-18 21:02:34 +0200

    fatJar task

--- src/main/java/StorageController.java
+++ src/main/java/StorageController.java
@@ -1,3 +1,11 @@
 public class StorageController {
 	public Root Root = new Root("Post AI Book");
+
+	public void save(String filePath){
+		
+	}
+	
+	public Root open(String filePath){
+		return null;
+	}
 }
