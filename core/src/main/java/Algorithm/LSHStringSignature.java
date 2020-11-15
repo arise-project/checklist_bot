275b49d2faf48bc266c7175c7e3b5685ffc1075c
arise-project <pirogov.e@gmail.com>
2020-11-15 13:58:46 -0500

    changet signature to set

--- core/src/main/java/Algorithm/LSHStringSignature.java
+++ core/src/main/java/Algorithm/LSHStringSignature.java
@@ -2,23 +2,19 @@ package Algorithm;
 
 import info.debatty.java.lsh.MinHash;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class LSHStringSignature {
-    public int [] getStringSignature(boolean [] vector) {
-        // Initialize the hash function for an similarity error of 0.1
-        // For sets built from a dictionary of 5 items
-        MinHash minhash = new MinHash(0.1, vector.length);
+    public int [] getStringSignature(Set<Integer> vector) {
+        MinHash minhash = new MinHash(0.1, vector.size());
         return minhash.signature(vector);
     }
 
-    public boolean [] getStringVector(String s) {
-        boolean [] result = new boolean[s.length()*32];
-        int index = 0;
+    public Set<Integer> getStringVector(String s) {
+        Set<Integer> result = new TreeSet<Integer>();
         for (int i = 0; i < s.length(); i++){
-            int c = s.charAt(i);
-            for (int j = 0; j < 32; ++j) {
-                result[index] = (c & (1 << j)) != 0;
-                index++;
-            }
+            result.add((int)s.charAt(i));
         }
 
         return result;
