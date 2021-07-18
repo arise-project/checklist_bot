fd9b42b44d0f10d351ed63bce449646cde8d5172
arise-project <pirogov.e@gmail.com>
2021-07-18 23:47:36 +0300

    fixed DI

---
+++ core/src/main/java/Command/Handler/ResetAttrCommandHandler.java
@@ -0,0 +1,28 @@
+package Command.Handler;
+
+import Command.Handler.Interface.IStorageCommandHandler;
+import Command.SaveStorageCommand;
+import Command.StorageCommandType;
+import Repository.Interface.IStorageRepository;
+import com.google.inject.Inject;
+
+public class SaveStorageCommandHandler implements IStorageCommandHandler<SaveStorageCommand> {
+    private final IStorageRepository storage;
+
+    @Inject
+    public SaveStorageCommandHandler(IStorageRepository storage){
+        this.storage = storage;
+    }
+
+    @Override
+    public void handle(SaveStorageCommand saveStorageCommand) {
+        if(saveStorageCommand.getRoot() != null) {
+            storage.save(saveStorageCommand.getFileName(), saveStorageCommand.getRoot());
+        }
+    }
+
+    @Override
+    public StorageCommandType getType() {
+        return StorageCommandType.SaveStorage;
+    }
+}
